487b0ebfab737093eadd9135a17b466fa46fb582
Bryan Siegel <bryansiegel@gmail.com>
2024-06-24 13:01:35 -0700

    Job Model

--- src/main/java/com/bryansiegel/ccsdjobs/models/Job.java
+++ src/main/java/com/bryansiegel/ccsdjobs/models/Job.java
@@ -9,6 +9,33 @@ import java.time.LocalDateTime;
 @Entity
 public class Job {
 
+    public Job() {
+    }
+
+    public Job(Long id, String jobTitle, String referenceCode, String division, String classification, String termsOfEmployment, String flaStatus, String applyLink, String positionSummary, String positionExpectations, String distinguishingCharacteristics, String knowledgeSkillsAndAbilities, String documentsRequiredAtTimeOfApplication, String examplesOfAssignedWorkAreas, String workEnvironment, String examplesOfEquipmentSuppliesUsedToPerformTasks, String essentialDutiesAndResponsibilities, String positionRequirements, String salaryAndBenefits, String aaEoeStatement, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.jobTitle = jobTitle;
+        this.referenceCode = referenceCode;
+        this.division = division;
+        this.classification = classification;
+        this.termsOfEmployment = termsOfEmployment;
+        this.flaStatus = flaStatus;
+        this.applyLink = applyLink;
+        this.positionSummary = positionSummary;
+        this.positionExpectations = positionExpectations;
+        this.distinguishingCharacteristics = distinguishingCharacteristics;
+        this.knowledgeSkillsAndAbilities = knowledgeSkillsAndAbilities;
+        this.documentsRequiredAtTimeOfApplication = documentsRequiredAtTimeOfApplication;
+        this.examplesOfAssignedWorkAreas = examplesOfAssignedWorkAreas;
+        this.workEnvironment = workEnvironment;
+        this.examplesOfEquipmentSuppliesUsedToPerformTasks = examplesOfEquipmentSuppliesUsedToPerformTasks;
+        this.essentialDutiesAndResponsibilities = essentialDutiesAndResponsibilities;
+        this.positionRequirements = positionRequirements;
+        this.salaryAndBenefits = salaryAndBenefits;
+        this.aaEoeStatement = aaEoeStatement;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -77,5 +104,207 @@ public class Job {
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getReferenceCode() {
+        return referenceCode;
+    }
+
+    public void setReferenceCode(String referenceCode) {
+        this.referenceCode = referenceCode;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public String getTermsOfEmployment() {
+        return termsOfEmployment;
+    }
+
+    public void setTermsOfEmployment(String termsOfEmployment) {
+        this.termsOfEmployment = termsOfEmployment;
+    }
+
+    public String getFlaStatus() {
+        return flaStatus;
+    }
+
+    public void setFlaStatus(String flaStatus) {
+        this.flaStatus = flaStatus;
+    }
+
+    public String getApplyLink() {
+        return applyLink;
+    }
+
+    public void setApplyLink(String applyLink) {
+        this.applyLink = applyLink;
+    }
+
+    public String getPositionSummary() {
+        return positionSummary;
+    }
+
+    public void setPositionSummary(String positionSummary) {
+        this.positionSummary = positionSummary;
+    }
+
+    public String getPositionExpectations() {
+        return positionExpectations;
+    }
+
+    public void setPositionExpectations(String positionExpectations) {
+        this.positionExpectations = positionExpectations;
+    }
+
+    public String getDistinguishingCharacteristics() {
+        return distinguishingCharacteristics;
+    }
+
+    public void setDistinguishingCharacteristics(String distinguishingCharacteristics) {
+        this.distinguishingCharacteristics = distinguishingCharacteristics;
+    }
+
+    public String getKnowledgeSkillsAndAbilities() {
+        return knowledgeSkillsAndAbilities;
+    }
+
+    public void setKnowledgeSkillsAndAbilities(String knowledgeSkillsAndAbilities) {
+        this.knowledgeSkillsAndAbilities = knowledgeSkillsAndAbilities;
+    }
+
+    public String getDocumentsRequiredAtTimeOfApplication() {
+        return documentsRequiredAtTimeOfApplication;
+    }
+
+    public void setDocumentsRequiredAtTimeOfApplication(String documentsRequiredAtTimeOfApplication) {
+        this.documentsRequiredAtTimeOfApplication = documentsRequiredAtTimeOfApplication;
+    }
+
+    public String getExamplesOfAssignedWorkAreas() {
+        return examplesOfAssignedWorkAreas;
+    }
+
+    public void setExamplesOfAssignedWorkAreas(String examplesOfAssignedWorkAreas) {
+        this.examplesOfAssignedWorkAreas = examplesOfAssignedWorkAreas;
+    }
+
+    public String getWorkEnvironment() {
+        return workEnvironment;
+    }
+
+    public void setWorkEnvironment(String workEnvironment) {
+        this.workEnvironment = workEnvironment;
+    }
+
+    public String getExamplesOfEquipmentSuppliesUsedToPerformTasks() {
+        return examplesOfEquipmentSuppliesUsedToPerformTasks;
+    }
+
+    public void setExamplesOfEquipmentSuppliesUsedToPerformTasks(String examplesOfEquipmentSuppliesUsedToPerformTasks) {
+        this.examplesOfEquipmentSuppliesUsedToPerformTasks = examplesOfEquipmentSuppliesUsedToPerformTasks;
+    }
+
+    public String getEssentialDutiesAndResponsibilities() {
+        return essentialDutiesAndResponsibilities;
+    }
+
+    public void setEssentialDutiesAndResponsibilities(String essentialDutiesAndResponsibilities) {
+        this.essentialDutiesAndResponsibilities = essentialDutiesAndResponsibilities;
+    }
+
+    public String getPositionRequirements() {
+        return positionRequirements;
+    }
+
+    public void setPositionRequirements(String positionRequirements) {
+        this.positionRequirements = positionRequirements;
+    }
+
+    public String getSalaryAndBenefits() {
+        return salaryAndBenefits;
+    }
+
+    public void setSalaryAndBenefits(String salaryAndBenefits) {
+        this.salaryAndBenefits = salaryAndBenefits;
+    }
+
+    public String getAaEoeStatement() {
+        return aaEoeStatement;
+    }
+
+    public void setAaEoeStatement(String aaEoeStatement) {
+        this.aaEoeStatement = aaEoeStatement;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" +
+                "id=" + id +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", referenceCode='" + referenceCode + '\'' +
+                ", division='" + division + '\'' +
+                ", classification='" + classification + '\'' +
+                ", termsOfEmployment='" + termsOfEmployment + '\'' +
+                ", flaStatus='" + flaStatus + '\'' +
+                ", applyLink='" + applyLink + '\'' +
+                ", positionSummary='" + positionSummary + '\'' +
+                ", positionExpectations='" + positionExpectations + '\'' +
+                ", distinguishingCharacteristics='" + distinguishingCharacteristics + '\'' +
+                ", knowledgeSkillsAndAbilities='" + knowledgeSkillsAndAbilities + '\'' +
+                ", documentsRequiredAtTimeOfApplication='" + documentsRequiredAtTimeOfApplication + '\'' +
+                ", examplesOfAssignedWorkAreas='" + examplesOfAssignedWorkAreas + '\'' +
+                ", workEnvironment='" + workEnvironment + '\'' +
+                ", examplesOfEquipmentSuppliesUsedToPerformTasks='" + examplesOfEquipmentSuppliesUsedToPerformTasks + '\'' +
+                ", essentialDutiesAndResponsibilities='" + essentialDutiesAndResponsibilities + '\'' +
+                ", positionRequirements='" + positionRequirements + '\'' +
+                ", salaryAndBenefits='" + salaryAndBenefits + '\'' +
+                ", aaEoeStatement='" + aaEoeStatement + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
