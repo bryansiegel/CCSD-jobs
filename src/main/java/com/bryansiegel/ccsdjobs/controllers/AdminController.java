384f8da76f8d9f48d7c9348c1f7f1e2f58dfe329
Bryan Siegel <bryansiegel@gmail.com>
2024-06-24 14:05:42 -0700

    basic jobs create

--- src/main/java/com/bryansiegel/ccsdjobs/controllers/AdminController.java
+++ src/main/java/com/bryansiegel/ccsdjobs/controllers/AdminController.java
@@ -1,12 +1,17 @@
 package com.bryansiegel.ccsdjobs.controllers;
 
+import com.bryansiegel.ccsdjobs.models.Job;
 import com.bryansiegel.ccsdjobs.repositories.JobsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminController {
@@ -19,12 +24,45 @@ public class AdminController {
     }
 
     //set limit on results
-    Pageable limit = PageRequest.of(0,5);
+    Pageable limit = PageRequest.of(0, 5);
 
-    @GetMapping("admin/dashboard")
+    @GetMapping("/admin/dashboard/")
     public String admin(Model model) {
         model.addAttribute("jobs", jobsRepository.findAll(limit));
         return "admin/dashboard.html";
     }
 
+    //jobs index
+    @GetMapping("/admin/jobs/")
+    public String jobs(Model model) {
+        model.addAttribute("jobs", jobsRepository.findAll(limit));
+        return "admin/jobs/index.html";
+    }
+
+    //jobs create
+    @GetMapping("/admin/jobs/create")
+    public String create(Model model) {
+        Job job = new Job();
+        model.addAttribute("jobs", job);
+        return "admin/jobs/create.html";
+    }
+
+    //jobs post create
+    @PostMapping("/admin/jobs/create")
+    public String postCreate(@ModelAttribute Job job, @RequestParam String jobTitle, BindingResult result) {
+
+        if(result.hasErrors())
+        {
+            return "admin/jobs/create.html";
+        }
+
+//        Job job = new Job();
+        jobsRepository.save(job);
+
+        return "redirect:/admin/jobs/";
+    }
+
+
+
+    //
 }
