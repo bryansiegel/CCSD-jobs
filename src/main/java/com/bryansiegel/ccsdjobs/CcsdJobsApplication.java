b48d015b010ba033e98d31381e50a52e4f9d47b1
Bryan Siegel <bryansiegel@gmail.com>
2024-06-24 11:35:14 -0700

    initial project structure and db setup

---
+++ src/main/java/com/bryansiegel/ccsdjobs/CcsdJobsApplication.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.ccsdjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcsdJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcsdJobsApplication.class, args);
+    }
+
+}
