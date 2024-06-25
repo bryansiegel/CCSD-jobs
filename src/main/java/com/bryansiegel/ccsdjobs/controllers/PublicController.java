a2777cf9cde0e50019437e90623d64da9826c026
Bryan Siegel <bryansiegel@gmail.com>
2024-06-25 09:05:29 -0700

    index design

--- src/main/java/com/bryansiegel/ccsdjobs/controllers/PublicController.java
+++ src/main/java/com/bryansiegel/ccsdjobs/controllers/PublicController.java
@@ -1,10 +1,12 @@
 package com.bryansiegel.ccsdjobs.controllers;
 
+import com.bryansiegel.ccsdjobs.models.Job;
 import com.bryansiegel.ccsdjobs.repositories.JobsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PublicController {
@@ -22,6 +24,17 @@ public class PublicController {
     private String index(Model model) {
         model.addAttribute("jobs", _jobsRepository.findAll());
         return "public/index";
+    }
+
+    //public job view
+    @GetMapping("/job/details/{id}")
+    private String jobDetails(@PathVariable Long id, Model model) {
+        Job job = _jobsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("jobs", job);
+
+        return "public/jobs/details";
+
     }
 
 }
